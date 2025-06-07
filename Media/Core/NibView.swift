e62bc22b033cac64b1abdedb8603c4f371dcae71
rlarjsdn3 <rlarjsdn3@naver.com>
2025-06-07 21:39:35 +0900

    feat: NibView 구현, TSAlertController 의존성 추가

---
+++ Media/Core/NibView.swift
@@ -0,0 +1,25 @@
+//
+//  NibView.swift
+//  Media
+//
+//  Created by 김건우 on 6/7/25.
+//
+
+import UIKit
+
+class NibView: UIView, NibLodable {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+        setupAttributes()
+    }
+
+    /// UI 요소들을 뷰 계층에 추가합니다.
+    func setupUI() {
+    }
+
+    /// 뷰의 속성(색상, 폰트 등)을 설정합니다.
+    func setupAttributes() {
+    }
+}
