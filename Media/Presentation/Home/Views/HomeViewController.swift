9e3eb3eb2286e7901dfa6282785abffe8da7f321
김재훈 <jaehun6165@naver.com>
2025-06-08 17:29:43 +0900

    [Feat] 검색 테이블 뷰 셀 생성 및 네비게이션 설정 - SearchTableViewCell.swift 생성 - Home 화면에서 검색 버튼 누르면 SearchViewController로 이동되도록 구현 - 관련 storyboard 설정 완료

---
+++ Media/Presentation/Home/Views/HomeViewController.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+final class HomeViewController: StoryboardViewController {
+
+
+    @IBAction func SearchButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "SearchViewController", bundle: nil)
+        if let searchVC = storyboard.instantiateViewController(withIdentifier: "SearchViewController") as? SearchViewController {
+            navigationController?.pushViewController(searchVC, animated: true)
+        }
+    }
+    
+    let service = DefaultDataTransferService()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
