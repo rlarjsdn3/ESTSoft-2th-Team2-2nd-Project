89a3e8e23bb647ac323b51a10610c451aa152bcd
rlarjsdn3 <rlarjsdn3@naver.com>
2025-06-09 11:29:57 +0900

    feat: 북마크 관련 Entity에 목 데이터 추가

--- Media/Model/CoreData/Playlist/PlaylistEntity+CoreDataProperties.swift
+++ Media/Model/CoreData/Playlist/PlaylistEntity+CoreDataProperties.swift
@@ -18,7 +18,7 @@ extension PlaylistEntity {
 
     @NSManaged public var name: String?
     @NSManaged public var createdAt: Date?
-    @NSManaged public var playlistVideos: NSOrderedSet?
+    @NSManaged public var playlistVideos: [PlaylistVideoEntity]?
 
 }
 
