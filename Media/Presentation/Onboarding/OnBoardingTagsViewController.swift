d1eb2f779e8807cda59869d650900e00b7c2a451
전광호 <haksang1002@gmail.com>
2025-06-09 15:16:56 +0900

    feat: 온보딩 파일 및 Tags CoreData 생성

---
+++ Media/Presentation/Onboarding/OnBoardingTagsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnBoardingTagsViewController.swift
+//  Media
+//
+//  Created by 전광호 on 6/9/25.
+//
+
+import UIKit
+
+class OnBoardingTagsViewController: StoryboardViewController {
+
+    @IBOutlet weak var TegsCollectionView: UICollectionView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
