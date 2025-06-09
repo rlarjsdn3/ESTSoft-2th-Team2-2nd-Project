a4226a87b30693b00e7424b14cf2e5c6b467ae9c
백현진 <guswlsrlacl@naver.com>
2025-06-09 09:59:55 +0900

    SearchVC, SearchResultVC 추가

---
+++ Media/Presentation/Search/SearchResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultViewController.swift
+//  Media
+//
+//  Created by 백현진 on 6/9/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Media/Presentation/Search/SearchViewController.swift
+++ Media/Presentation/Search/SearchViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 final class SearchViewController: StoryboardViewController {
-
+    @IBOutlet weak var searchTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
     }
+
+    override func setupUI() {
+        configureSearchTableView()
+    }
+
+    override func setupAttributes() {
+    }
+
+    private func configureSearchTableView() {
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+    }
+
+}
+
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+}
+
+extension SearchViewController: UITableViewDelegate {
+
 }
