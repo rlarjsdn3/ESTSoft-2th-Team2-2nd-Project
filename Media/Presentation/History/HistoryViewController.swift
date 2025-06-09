5b8e89e51e125ec0542ba0e19d4dd98a24a737db
Heejung Yang <wolla39@gmail.com>
2025-06-09 11:42:30 +0900

    HistoryViewController 생성

---
+++ Media/Presentation/History/HistoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryViewController.swift
+//  Media
+//
+//  Created by Heejung Yang on 6/9/25.
+//
+
+import UIKit
+
+class HistoryViewController: StoryboardViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func setupHierachy() {
+        super.setupHierachy()
+    }
+
+    override func setupAttributes() {
+        super.setupAttributes()
+    }
+
+}
