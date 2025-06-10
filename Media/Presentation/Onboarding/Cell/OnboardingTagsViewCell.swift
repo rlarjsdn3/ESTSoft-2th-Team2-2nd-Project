fad6bdf597f6bdb2504bd275d53be39b2f21f425
전광호 <haksang1002@gmail.com>
2025-06-10 18:12:09 +0900

    온보딩 화면 디테일 추가 및 앱 내의 카테고리 뷰 파일 생성

---
+++ Media/Presentation/Onboarding/Cell/OnboardingTagsViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingTegsViewCell.swift
+//  Media
+//
+//  Created by 전광호 on 6/9/25.
+//
+
+import UIKit
+
+class OnboardingTagsViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tagsImageView: UIImageView!
+    
+    @IBOutlet weak var TagsTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.primary.cgColor
+        layer.cornerRadius = 20
+        contentView.layer.borderColor = UIColor.primary.cgColor
+        clipsToBounds = true
+    }
+}
