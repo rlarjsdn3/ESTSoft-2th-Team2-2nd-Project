c7b7bb98df124129db6b5496445320b6eaf82bdd
Heejung Yang <wolla39@gmail.com>
2025-06-10 17:02:28 +0900

    재생기록/북마크 리스트를 위한 MediumVideoCell 생성

---
+++ Media/Presentation/History/Cell/MediumVideoCell.swift
@@ -0,0 +1,17 @@
+//
+//  MediumVideoCell.swift
+//  Media
+//
+//  Created by Heejung Yang on 6/10/25.
+//
+
+import UIKit
+
+class MediumVideoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
