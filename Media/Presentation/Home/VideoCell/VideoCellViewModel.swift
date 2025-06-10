8cde58ce3ab7eb08860dd8048fc84851ad3e85f2
김재훈 <jaehun6165@naver.com>
2025-06-10 17:18:55 +0900

    [Feat] Home화면 카테고리 버튼 기능 활성화 및 비디오 연결, 비디오셀 디자인 수정

--- Media/Presentation/Home/VideoCell/VideoCellViewModel.swift
+++ Media/Presentation/Home/VideoCell/VideoCellViewModel.swift
@@ -7,5 +7,27 @@ struct VideoCellViewModel {
     let durationText: String
     let thumbnailURL: URL?
     let profileImageURL: URL?
+    let likeCountText: String
+    let categoryText: String
+
+    init(
+        title: String,
+        viewCountText: String,
+        durationText: String,
+        thumbnailURL: URL?,
+        profileImageURL: URL?,
+        likeCountText: String,
+        tags: String
+    ) {
+        self.title = title
+        self.viewCountText = viewCountText
+        self.durationText = durationText
+        self.thumbnailURL = thumbnailURL
+        self.profileImageURL = profileImageURL
+        self.likeCountText = likeCountText
+        // 태그값을 카테고리로 변환하기위하여
+        let components = tags.split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+            self.categoryText = components.first?.capitalized ?? "Unknown"
+    }
 }
  
