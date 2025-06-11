69d61e647b5a91f164c3ccb005498439bbef9cf3
Heejung Yang <wolla39@gmail.com>
2025-06-11 10:18:05 +0900

    북마크/재생목록 모두보기 화면을 위한 MediumVideoCell 생성

--- Media/Presentation/History/Cell/MediumVideoCell.swift
+++ Media/Presentation/History/Cell/MediumVideoCell.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class MediumVideoCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+
+    @IBOutlet weak var tagsLabel: UILabel!
+
+    @IBOutlet weak var userNameLabel: UILabel!
+
+    @IBOutlet weak var viewsCountLabel: UILabel!
+
+    @IBOutlet weak var actionButton: UIButton!
+
+    @IBAction func tapButton(_ sender: Any) {
     }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    private func setViews() {
+        thumbnailImageView.layer.borderWidth = 2
+        thumbnailImageView.layer.borderColor = UIColor.white.cgColor
+        thumbnailImageView.layer.cornerRadius = 8
+    }
+
+}
+
+extension MediumVideoCell {
+    func configure() {
+
+    }
 }
