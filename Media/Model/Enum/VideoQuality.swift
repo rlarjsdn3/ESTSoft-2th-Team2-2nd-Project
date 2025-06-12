728eedd4cbe0242b83b91b789828af239dc802b7
Minji Kang <aldalddl2007@gmail.com>
2025-06-13 00:49:29 +0900

    feat: VideoQuality 열거형 정의

---
+++ Media/Model/Enum/VideoQuality.swift
@@ -0,0 +1,13 @@
+//
+//  VideoQuality.swift
+//  Media
+//
+//  Created by 강민지 on 6/13/25.
+//
+
+enum VideoQuality: String, CaseIterable {
+    case large = "Large"
+    case medium = "Medium"
+    case small = "Small"
+    case tiny = "Tiny"
+}
