ce378ff9dbba5e365082714f9d92721cc804755d
전광호 <haksang1002@gmail.com>
2025-06-13 21:17:22 +0900

    fix: 다크모드 데이터 저장 복구

--- Media/Application/SceneDelegate.swift
+++ Media/Application/SceneDelegate.swift
@@ -22,6 +22,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        if let windowScene = scene as? UIWindowScene,
+           let window = windowScene.windows.first {
+            let isDark = UserDefaults.standard.bool(forKey: "isDarkMode")
+            window.overrideUserInterfaceStyle = isDark ? .dark : .light
+        }
+        
         let window = UIWindow(windowScene: windowScene)
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let onboardingStoryboard = UIStoryboard(name: "OnBoardingOnBoardingViewController", bundle: nil)
