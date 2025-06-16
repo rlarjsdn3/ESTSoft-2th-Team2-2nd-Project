d735541814dbc8fd597616b320c8d47b94f5ecde
전광호 <haksang1002@gmail.com>
2025-06-16 17:04:02 +0900

    Setting View에서 카테고리 선택시 TagsView에도 UI 동기화되게 수정, 아이폰 상태일시, 다크모드, 셀 선택에 따른 색상변경

--- Media/Presentation/SelectedCategory/SelectedTagsViewController.swift
+++ Media/Presentation/SelectedCategory/SelectedTagsViewController.swift
@@ -18,7 +18,7 @@ class SelectedTagsViewController: StoryboardViewController {
         showAlert("Notification", message: "\(selectedCategories.count) categories have been selected.") { _ in
             /// 확인 버튼을 눌렀을 경우
             // 태그 코어데이터의 모든 데이터를 삭제
-            TagsDataManager.shared.deleteAll()
+            TagsDataManager.shared.deleteAllTagsData()
             
             // 선택한 태그들을 코어데이터에 저장
             for category in self.selectedCategories {
@@ -34,12 +34,14 @@ class SelectedTagsViewController: StoryboardViewController {
             
             self.dismiss(animated: true)
         } onCancel: { _ in
+            #warning("전광호 -> 수정해야 됨!!!!!!!!!!!!!!!!!!!!!")
             /// 취소버튼을 눌렀을 경우
             // 선택된 모든 셀의 선택을 해제하고 기본색상으로 설정
             for indexPath in self.selectIndexPath {
                 self.tagsCollectionView.deselectItem(at: indexPath, animated: false)
                 if let cell = self.tagsCollectionView.cellForItem(at: indexPath) as? SelectedTagsViewControllerCell {
-                    cell.contentView.backgroundColor = .tagBorder
+//                    cell.contentView.backgroundColor = .tagBorderColorAlpha
+                    self.updateCellAppearance(cell, selected: false)
                 }
             }
             
@@ -51,7 +53,8 @@ class SelectedTagsViewController: StoryboardViewController {
             for indexPath in self.selectIndexPath {
                 self.tagsCollectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
                 if let cell = self.tagsCollectionView.cellForItem(at: indexPath) as? SelectedTagsViewControllerCell {
-                    cell.contentView.backgroundColor = .tagSelected
+//                    cell.contentView.backgroundColor = .tagSelected
+                    self.updateCellAppearance(cell, selected: true)
                 }
             }
             
@@ -72,10 +75,27 @@ class SelectedTagsViewController: StoryboardViewController {
     
     var backUpCategories: [Category] = []
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Onboarding, Setting View에서 선택된 카테고리 다시 로드
+        selectedCategories = TagsDataManager.shared.fetchSelectedCategories()
+        
+        // IndexPath 재구성
+        selectIndexPath.removeAll()
+        
+        for (index, tag) in tags.enumerated() {
+            if selectedCategories.contains(tag) {
+                selectIndexPath.insert(IndexPath(item: index, section: 0))
+            }
+        }
+
+        // UI 갱신
+        tagsCollectionView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        selectedCategories = TagsDataManager.shared.fetchSelectedCategories()
         
         for (index, tag) in tags.enumerated() {
             if selectedCategories.contains(tag) {
@@ -87,8 +107,6 @@ class SelectedTagsViewController: StoryboardViewController {
         backUpCategories = selectedCategories
         backUpIndexPath = selectIndexPath
         
-        tagsCollectionView.reloadData()
-        
         setUpLayout()
         
         tagsCollectionView.allowsMultipleSelection = true
@@ -99,6 +117,10 @@ class SelectedTagsViewController: StoryboardViewController {
         DispatchQueue.main.async {
             for indexPath in self.selectIndexPath {
                 self.tagsCollectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+                
+                if let cell = self.tagsCollectionView.cellForItem(at: indexPath) as? SelectedTagsViewControllerCell {
+                    cell.contentView.backgroundColor = .tagSelected
+                }
             }
         }
         
@@ -109,18 +131,18 @@ class SelectedTagsViewController: StoryboardViewController {
     func setUpLayout() {
         // item 사이즈
         let itemSize: NSCollectionLayoutSize
+        let groupSize: NSCollectionLayoutSize
         
         // 디바이스 정보에 따라 카테고리 아이템 크기 분기
         if traitCollection.userInterfaceIdiom == .phone {
-            itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.47), heightDimension: .absolute(150))
+            itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.47), heightDimension: .fractionalWidth(0.47))
+            groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.47))
         } else {
             itemSize = NSCollectionLayoutSize(widthDimension: .absolute(160), heightDimension: .absolute(160))
+            groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(160))
         }
         
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        // 그룹 사이즈
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(150))
         
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         
@@ -144,6 +166,19 @@ class SelectedTagsViewController: StoryboardViewController {
             selectedTagsButton.isEnabled = false
         }
     }
+    
+    // 다크모드와 셀 선택에 따른 셀의 색상변경
+    func updateCellAppearance(_ cell: SelectedTagsViewControllerCell, selected: Bool) {
+        if selected {
+            cell.contentView.backgroundColor = .tagSelected
+            cell.tagsTitle.textColor = traitCollection.userInterfaceStyle == .dark ? .black : .white
+            cell.tagsImageView.tintColor = traitCollection.userInterfaceStyle == .dark ? .black : .white
+        } else {
+            cell.contentView.backgroundColor = .tagBorderColorAlpha
+            cell.tagsTitle.textColor = .label
+            cell.tagsImageView.tintColor = .label
+        }
+    }
 }
 
 extension SelectedTagsViewController: UICollectionViewDataSource {
@@ -155,15 +190,15 @@ extension SelectedTagsViewController: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedTagsViewControllerCell", for: indexPath) as? SelectedTagsViewControllerCell else { return UICollectionViewCell() }
         
         let target = tags[indexPath.item]
-        
-        if selectIndexPath.contains(indexPath) {
-            cell.contentView.backgroundColor = .tagSelected
-        } else {
-            cell.contentView.backgroundColor = .tagBorder
-        }
-        
+
         cell.tagsTitle.text = target.rawValue.capitalized
         cell.tagsImageView.image = target.symbolImage
+        
+        if selectIndexPath.contains(indexPath) {
+            updateCellAppearance(cell, selected: true)
+        } else {
+            updateCellAppearance(cell, selected: false)
+        }
         
         return cell
     }
@@ -175,7 +210,7 @@ extension SelectedTagsViewController: UICollectionViewDelegate {
         print("셀 선택")
         if selectIndexPath.count >= 5 {
             
-            showAlert("🔔Notification", message: "Only up to 5 categories can be selected") { _ in
+            showAlert("❌Can't select more than 5", message: "Only up to 5 categories can be selected") { _ in
                 self.dismiss(animated: true)
             } onCancel: { _ in
                 self.dismiss(animated: true)
@@ -191,7 +226,7 @@ extension SelectedTagsViewController: UICollectionViewDelegate {
         
         // 셀 선택시 색상변경
         if let cell = collectionView.cellForItem(at: indexPath) as? SelectedTagsViewControllerCell {
-            cell.contentView.backgroundColor = .tagSelected
+            updateCellAppearance(cell, selected: true)
         }
         
         buttonIsEnabled()
@@ -204,12 +239,9 @@ extension SelectedTagsViewController: UICollectionViewDelegate {
         print("셀 선택 해제")
         // 셀 선택해제 시 색상변경
         if let cell = collectionView.cellForItem(at: indexPath) as? SelectedTagsViewControllerCell {
-            cell.contentView.backgroundColor = .tagBorder
+            updateCellAppearance(cell, selected: false)
         }
         
         buttonIsEnabled()
     }
 }
-
-
-
