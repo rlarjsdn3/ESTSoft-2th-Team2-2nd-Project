189d0b3c880f2f711289be7fa669e0ad1565e5f0
rlarjsdn3 <rlarjsdn3@naver.com>
2025-06-16 13:56:15 +0900

    move: PIPPlayerViewController를 다른 파일로 이동

--- Media/Infrastructure/VideoPlayer/VideoPlayerService.swift
+++ Media/Infrastructure/VideoPlayer/VideoPlayerService.swift
@@ -306,16 +306,3 @@ extension DefaultVideoPlayerService: VideoPlayerService {
     }
 
 }
-
-final class PiPEnabledPlayerViewController: AVPlayerViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // PiP 기능 활성화 (영상이 작은 창으로 재생되도록 허용)
-        self.allowsPictureInPicturePlayback = true
-        // 사용자가 별도로 PiP 버튼을 누르지 않아도
-        // 앱이 백그라운드로 전환될 때 자동으로 PiP 모드를 시작할 수 있도록 허용
-        self.canStartPictureInPictureAutomaticallyFromInline = true
-    }
-}
