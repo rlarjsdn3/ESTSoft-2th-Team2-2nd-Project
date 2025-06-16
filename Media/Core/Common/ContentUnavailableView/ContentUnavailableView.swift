881af579ffd48e4771c423d14b33633f1d7b3193
rlarjsdn3 <rlarjsdn3@naver.com>
2025-06-16 10:59:24 +0900

    fix: ContentUnavailableView의 중앙 이미지 크기 비율 수정

--- Media/Core/Common/ContentUnavailableView/ContentUnavailableView.swift
+++ Media/Core/Common/ContentUnavailableView/ContentUnavailableView.swift
@@ -12,14 +12,33 @@ final class ContentUnavailableView: NibView {
     /// 사용자에게 콘텐츠가 없음을 나타내는 이미지를 표시하는 뷰입니다.
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+
     /// 표시할 이미지 리소스를 나타내는 속성입니다.
     /// 설정 시 자동으로 `imageView`의 이미지를 업데이트합니다.
     var imageResource: ImageResource = .noVideos {
         didSet { imageView.image = UIImage(resource: imageResource) }
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadFromNib(owner: self)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let viewWidth = self.bounds.width
+        let viewHeight = self.bounds.height
+
+        if traitCollection.horizontalSizeClass == .compact {
+            imageWidthConstraint.constant = viewWidth * 0.75
+            imageHeightConstraint.constant = viewHeight * 0.75
+        } else {
+            imageWidthConstraint.constant = viewWidth * 0.5
+            imageHeightConstraint.constant = viewHeight * 0.5
+
+        }
     }
 }
